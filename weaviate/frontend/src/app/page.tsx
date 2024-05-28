b91c35a7137e5a63ac47223a7d5433d1ebd3d4a8
Vincent Nahn <vincentnahn@gmail.com>
2024-05-28 18:00:00 +0800

    Final product

--- weaviate/frontend/src/app/page.tsx
+++ weaviate/frontend/src/app/page.tsx
@@ -41,81 +41,91 @@ function App() {
     event.preventDefault();
   };
 
-  const getResults = (results: any) => {
-    // if (!results) return;
-    const head = results['data']['Get']['MultiModal'][0];
-    if (head)
-      return <div className="flex items-center justify-center flex-col">
-        <Image
-          height={200}
-          width={800}
-          alt="Certainty: "
-          src={
-            'data:image/jpg;base64,' +
-            head['image']
-          }
-        />
-        <div >Certainty: {(head['_additional']['certainty'] * 100).toFixed(2)} %</div>
-      </div>
-  }
-
-  const getRestResults = (results: any) => {
-    // if (!results) return;
-    const [, ...rest] = results['data']['Get']['MultiModal'];
-    // if (rest)
-    return <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', paddingTop: "20px" }}>
-      {
-        rest.map((obj: any) => {
-          return (
-            <div key={obj['_additional']['id']}>
-              <Image
-                height={5000}
-                width={400}
-                alt="Certainty: "
-                src={
-                  'data:image/jpg;base64,' +
-                  obj['image']
-                }
-              />
-              <div >Certainty: {(obj['_additional']['certainty'] * 100).toFixed(2)} %</div>
-            </div>
-          )
-        })
-      }
-    </div>
-  }
 
   // if (results?.data)
   return (
-    <div className="container pt-8" >
-      <h1 className="text-3xl font-bold">
-        XKCD Comic Search
-      </h1>
-      <form
-        onSubmit={onSubmit}
-      >
-        <div className="flex flex-row my-4">
-          <Input
-            className="mr-4"
-            type="text"
-            placeholder="Search for images"
-            onChange={onChange}
-          />
-          <Button
-            className="font-bold"
-            type="submit"
-            value="Search"
-            style={{ backgroundColor: '#fa0171' }}
-          >Search</Button>
-        </div>
-      </form>
-      {results.data && getResults(results)}
-      {results.data &&
-        <div className="control" style={{ paddingTop: "20px" }}>
-        </div>}
-      {results.data && getRestResults(results)}
-      <div style={{ height: '50px' }} />
-    </div>
+    <>
+      {!results.data && <div className="absolute w-screen h-screen -z-10">
+        <Image
+          src="/background_image.webp"
+          alt="background"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>}
+      <div className="container pt-8" >
+        <h1 className="text-3xl font-bold">
+          XKCD Comic Search
+        </h1>
+        <form
+          onSubmit={onSubmit}
+        >
+          <div className="flex flex-row my-4">
+            <Input
+              className="mr-4"
+              type="text"
+              placeholder="Search for images"
+              onChange={onChange}
+            />
+            <Button
+              className="font-bold"
+              type="submit"
+              value="Search"
+              style={{ backgroundColor: '#fa0171' }}
+            >Search</Button>
+          </div>
+        </form>
+        <Results results={results} />
+        <RestResults results={results} />
+        <div style={{ height: '50px' }} />
+      </div>
+    </>
   );
 }
+const Results = ({ results }: { results: any }) => {
+  if (!results?.data) return <></>;
+  const head = results['data']['Get']['MultiModal'][0];
+  if (head)
+    return <div className="flex items-center justify-center flex-col h-96 relative my-4">
+      <Image
+        fill
+        objectFit="contain"
+        src={
+          'data:image/webp;base64,' +
+          head['image']
+        }
+        alt="Best image"
+      />
+      <div >Certainty: {(head['_additional']['certainty'] * 100).toFixed(2)} %</div>
+    </div>
+}
+
+const RestResults = ({ results }: { results: any }) => {
+  if (!results?.data) return <></>;
+  const [, ...rest] = results['data']['Get']['MultiModal'];
+  // if (rest)
+  return <div className="flex flex-wrap justify-between gap-y-4 gap-x-2">
+    {
+      rest.map((obj: any) => {
+        return (
+          <div key={obj['_additional']['id']} >
+            <img
+              style={{ maxHeight: '200px' }}
+              alt="Image"
+              loading="lazy"
+              src={
+                'data:image/webp;base64,' +
+                obj['image']
+              }
+            />
+            <div >Certainty: {(obj['_additional']['certainty'] * 100).toFixed(2)} %</div>
+          </div>
+        )
+      })
+    }
+  </div>
+}
+
 export default App;
+
